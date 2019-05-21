d2803a3100705e7aa65afec467fc48fdc999344f
neramuca <neramuca@gmail.com>
2019-05-21 14:51:47 +0200

    Update: 2019-05-06 - Pruebas de conexion Notebook.ipynb FMT_Data_TobeProcessed_Sellin.ipynb FMT_Data_TobeProcessed_Sellout.ipynb FMT_Data_ValidateFiles.ipynb FMT_File_MoveToLanding_FTP.ipynb FMT_File_MoveToLanding_Unzip.ipynb MDM_SP_Data_TobeProcessed.ipynb MDM_SP_Data_ValidateFiles.ipynb MDM_SP_File_MoveToLanding_FTP.ipynb README.md notebooks

---
+++ notebooks/Users/emunozcar@dragosolutions.com/Pruebas/2019-05-06 - Pruebas de conexion Notebook (2).py
@@ -0,0 +1,38 @@
+# Databricks notebook source
+storage_account_name = "Nombre del storage"
+storage_account_access_key = "clave de acceso al storage"
+
+# COMMAND ----------
+
+file_location = "wasbs://practica001/Multas/"
+file_type = "csv"
+
+# COMMAND ----------
+
+spark.conf.set(
+  "fs.azure.account.key."+storage_account_name+".blob.core.windows.net",
+  storage_account_access_key)
+
+
+# COMMAND ----------
+
+df = spark.read.format("csv").option("header", "true").option("delimiter", ";").load("wasbs://practica001@strge0001.blob.core.windows.net/Multas")
+
+dbutils.fs.ls("wasbs://practica001@strge0001.blob.core.windows.net/Multas")
+
+
+
+# COMMAND ----------
+
+# ESTA ES UNA PRUEBA
+display(df)
+
+# COMMAND ----------
+
+#Branch Ana adicion 01
+display(df)
+
+# COMMAND ----------
+
+#branch Ana Adicion 02
+display(df)
