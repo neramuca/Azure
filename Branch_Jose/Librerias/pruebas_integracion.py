022811af89e4bf5b7620e9bb0385aefbd228c821
neramuca <neramuca@gmail.com>
2019-06-11 10:01:55 +0200

    Update: Branch_ANA Branch_Jose Librerias Procesos README.md

---
+++ Branch_Jose/Librerias/pruebas_integracion.py
@@ -0,0 +1,33 @@
+# Databricks notebook source
+#ESTE NOTEBOOK SE HACE PARA REALIZAR PRUEBAS DE INTEGRACION
+
+# COMMAND ----------
+
+#mensaje agregado por Ana DÍA 22052019
+#sEGUNDO MENSAJE
+
+# COMMAND ----------
+
+#mensaje Agregado por Victor
+
+# COMMAND ----------
+
+#datos nuevos agregados por ANA
+
+# COMMAND ----------
+
+print("mensaje de ANA")
+
+# COMMAND ----------
+
+#Hola Mundo
+
+print("mensaje 03062019" )
+
+# COMMAND ----------
+
+
+print("Mensaje 04062019")
+
+print("Nuevo mensaje de Victor para el archivo")
+mensaje de Jose
