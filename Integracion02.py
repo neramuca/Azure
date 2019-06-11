ff7469025a71e7605d50de6a5f0afc0ef96611de
neramuca <neramuca@gmail.com>
2019-06-11 12:35:14 +0200

    Se agregan al master  Mensaje 05-06-07

--- Integracion02.py
+++ Integracion02.py
@@ -1,14 +1,28 @@
 # Databricks notebook source
-#Archivo de base para pruebas
+#Archivo de base para pruebas#Archivo de base para pruebas
 
 # COMMAND ----------
 
-#Mensaje 01
+#Mensaje 01#Mensaje 01
 
 # COMMAND ----------
+
+#Mensaje 02 - Ana
 
 #Mensaje 03 - master
 
 # COMMAND ----------
 
 #Mensaje 04 - Master
+
+# COMMAND ----------
+
+#M 05 master
+
+# COMMAND ----------
+
+#M 06 master
+
+# COMMAND ----------
+
+#M 07 Master
